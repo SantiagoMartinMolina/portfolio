832c0f4473e2892f2869cc344f774a10e8d007e3
Santiago Molina <molina.santiago.martin@gmail.com>
2021-04-30 22:03:12 -0300

    icons

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { StyledSkills } from './StyledSkills';
 import { NavLink } from 'react-router-dom';
-// import { ReactJs } from '@icons-pack/react-simple-icons'
-
+import ReactIcon from '../../icons/ReactIcon';
+import HTMLIcon from '../../icons/HTMLIcon';
+import CSSIcon from '../../icons/CSSIcon';
+import JavascriptIcon from '../../icons/JavascriptIcon';
+import ReduxIcon from '../../icons/ReduxIcon';
+import NodeIcon from '../../icons/NodeIcon';
+import ExpressIcon from '../../icons/ExpressIcon';
+import PostgreSQLIcon from '../../icons/PostgreSQL';
 
 const Skills = () => {
     return (
         <StyledSkills>
             <h2>Habilidades</h2>
-            {/* <ReactJs /> */}
+            <div>
+                <HTMLIcon />
+                <CSSIcon />
+                <JavascriptIcon />
+                <ReactIcon />
+                <ReduxIcon />
+                <NodeIcon />
+                <ExpressIcon />
+                <PostgreSQLIcon />
+            </div>
         </StyledSkills>
     )
 }
--- src/Components/Skills/StyledSkills.jsx
+++ src/Components/Skills/StyledSkills.jsx
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const StyledSkills = styled.section`
 background-color: var(--color2);
 padding: 2em;
+img{
+    color: red;
+}
 `

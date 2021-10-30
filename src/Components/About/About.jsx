26ce746ce7a40b24bcdf4c7868cb17fdf6fd3560
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2021-10-30 13:25:52 -0300

    fix about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <StyledAbout>
             <h1 datatext="Santiago Molina">Santiago Molina</h1>
             <p className='subtitle'>Fullstack developer</p>
-            <p className='text'>Hola! Soy desarrollador en javascript y estudiante de ingeniería en sistemas. Me apasiona la tecnología, el desarrollo frontend y backend. Comencé de forma autodidacta y luego cursé un bootcamp de 700 hs de estudio donde aprendí principalmente Javascript, React, Node, Express y SQL mediante la creación de diversos proyectos. Actualmente estoy en busqueda de mi primer trabajo en el ambiente IT.</p>
+            <p className='text'>Hola! Soy desarrollador en javascript y estudiante de ingeniería en sistemas. Me apasiona la tecnología, el desarrollo frontend y backend. Comencé de forma autodidacta y luego cursé un bootcamp de 700 hs de estudio donde aprendí principalmente Javascript, React, Node, Express y SQL mediante la creación de diversos proyectos.</p>
             <a className='btn' href="Santiago_molina_CV.pdf" target='_blank' rel='noopener noreferrer'>Descargar CV</a>
 
         </StyledAbout>
